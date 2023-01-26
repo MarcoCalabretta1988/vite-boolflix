2beeedb8bb73eee467377d6057bd14afc3302a64
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-01-26 15:31:46 +0100

    Add Series list

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,25 @@ export default {
         .catch(error => { console.log(error) })
         .then(() => { store.isLoading = false; });
     },
+    fetchTvSeries(query) {
+      store.isLoading = true;
+
+      const config = {
+        params: {
+          api_key: apiKey,
+          query,
+          language: 'it'
+        }
+
+      }
+
+      axios.get(`${apiUri}/search/tv`, config)
+        .then(res => { store.tvSeries = res.data.results; })
+        .catch(error => { console.log(error) })
+        .then(() => { store.isLoading = false; });
+    },
     buildImagePatch(data) {
+      if (!data) data = 'not'
       const url = new URL(`./assets/img/${data}.png`, import.meta.url)
       console.log(url.href)
       return url.href
@@ -41,13 +59,16 @@ export default {
 <template>
   <div class="container m-5 bg-dark p-5">
 
+    <!-- INPUT DI RICERCA -->
     <div class="input-group mb-3">
       <input type="text" class="form-control" placeholder="Cerca film o serie" v-model="searchTerm"
-        @keyup.enter="fetchMovies(searchTerm)">
+        @keyup.enter="fetchMovies(searchTerm), fetchTvSeries(searchTerm)">
       <button class="btn btn-outline-danger" type="button" id="button-addon2"
         @click="fetchMovies(searchTerm)">Cerca</button>
     </div>
 
+    <!-- LISTA FILM -->
+    <h1 class="text-center text-white">FILM:</h1>
     <ul class="list-group my-3 text-center" v-for="movie in store.movies" @key="movies.id">
       <li class="list-group-item">Titolo: {{ movie.title }}</li>
       <li class="list-group-item">Titolo Originale: {{ movie.original_title }}</li>
@@ -57,7 +78,21 @@ export default {
       <li class="list-group-item">Voto: {{ movie.vote_average }}</li>
       <li class="list-group-item">Trama: {{ movie.overview }}</li>
     </ul>
+
+    <!-- LISTA SERIE -->
+    <h1 class="text-center text-white">SERIE:</h1>
+    <ul class="list-group my-3 text-center" v-for="serie in store.tvSeries" @key="movies.id">
+      <li class="list-group-item">Titolo: {{ serie.name }}</li>
+      <li class="list-group-item">Titolo Originale: {{ serie.original_title }}</li>
+      <li class="list-group-item">Lingua: <img :src="buildImagePatch(serie.original_language)" :alt="serie.name"
+          class="img-fluid">
+      </li>
+      <li class="list-group-item">Voto: {{ serie.vote_average }}</li>
+      <li class="list-group-item">Trama: {{ serie.overview }}</li>
+    </ul>
+
   </div>
+
 </template>
 
 <style lang="scss">
